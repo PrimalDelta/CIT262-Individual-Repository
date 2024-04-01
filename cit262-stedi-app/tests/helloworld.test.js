f15426deb8792001b7b5f23dac70cdf67fcb4d8e
PrimalDelta <bro20073@byui.edu>
2024-04-01 17:35:18 -0600

    added data.json

--- cit262-stedi-app/tests/helloworld.test.js
+++ cit262-stedi-app/tests/helloworld.test.js
@@ -1,6 +1,8 @@
 import hello from "../utils/helloworld.js";
 
-it("Should say hello",()=>{
+it("Should say Jared",()=>{
     const helloString = hello();
     console.log(helloString);
+
+    expect(helloString).toBe("Jared")
 })
